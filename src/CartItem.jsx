bbf7b789ce2a852be8f4c512c8f57f5d1213cc3b
neooo <wskneo@gmail.com>
2025-05-23 22:44:55 -0400

    task3

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -8,28 +8,53 @@ const CartItem = ({ onContinueShopping }) => {
   const dispatch = useDispatch();
 
   // Calculate total amount for all products in the cart
+
   const calculateTotalAmount = () => {
- 
+    let total = 0;
+  
+    cart.forEach(item => {
+      const quantity = item.quantity;
+      const cost = parseFloat(item.cost.substring(1));
+      total += quantity * cost;
+    });
+  
+    return total;
   };
+  
 
   const handleContinueShopping = (e) => {
-   
+   return onContinueShopping(e);
+  };
+
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
   };
 
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+    if(item.quantity > 1){
+        dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+    }else{
+        dispatch(removeItem({ name: item.name}))
+    }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem({ name: item.name}))
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    const quantity = item.quantity;
+    const cost = parseFloat(item.cost.substring(1));
+    total += quantity * cost;
+    return total;
+
   };
 
   return (
